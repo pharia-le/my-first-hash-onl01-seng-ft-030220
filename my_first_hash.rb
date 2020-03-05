2ac5debd6006ff2035f9e68d11652e007365f9ff
Pharia Le <pharia.le@gmail.com>
2020-03-05 08:58:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,11 +12,6 @@ def shipping_manifest
     "whale bone corsets" => 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
-  }
-  # }
-  # the_manifest["whale bone corsets"] = 5
-  # the_manifest["porcelain vases"] = 2
-  # the_manifest["oil paintings"] = 3
 end
 
 def retrieval
@@ -27,6 +22,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
